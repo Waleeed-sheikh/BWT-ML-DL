254b1567c5ab4f5bc9ec6a341277a4338e40ea66
WALEED SHEIKH <146780047+Waleeed-sheikh@users.noreply.github.com>
2024-06-23 21:59:23 +0500

    Create QUESTION-2.py

---
+++ WEEK-2/QUESTION-2.py
@@ -0,0 +1,14 @@
+
+def Is_even(number):
+    if number%2==0:
+        return True
+    else:
+        return False
+
+if(Is_even(6)):
+    print("the number is even ")
+else:
+    print(" The number is odd")
+
+#not much to write  straight forward question 
+# we can make it more simple by simply sayin return number%2==0 but i thought the above method will be more easily understandable
